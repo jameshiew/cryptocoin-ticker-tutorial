4cb168a477461e5998ff4c3fcdf8a96802edec34
James Hiew <james@hiew.net>
2018-05-28 11:34:44 +0100

    Integrate Celery into our Django project

---
+++ ccticker/ccticker/celery.py
@@ -0,0 +1,16 @@
+# set the default Django settings module for the 'celery' program.
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccticker.settings')
+
+app = Celery('ccticker')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
