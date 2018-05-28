e2db319b79f67deef2ad737ac29b73f73561c37a
James Hiew <james@hiew.net>
2018-05-28 13:16:59 +0100

    Send updated prices via the WebSockets

--- ccticker/ccapp/consumers.py
+++ ccticker/ccapp/consumers.py
@@ -12,6 +12,10 @@ class TickerConsumer(WebsocketConsumer):
         cryptocoin = self.scope['url_route']['kwargs']['cryptocoin']
         currency = self.scope['url_route']['kwargs']['currency']
         self.ticker_code = cryptocoin + currency
+        async_to_sync(self.channel_layer.group_add)(
+            self.ticker_code,
+            self.channel_name
+        )
         super().connect()
         self.send(text_data=json.dumps({
             'message': f'connected'
@@ -19,6 +23,14 @@ class TickerConsumer(WebsocketConsumer):
         self.price_update({
             'price': cache.get(self.ticker_code)
         })
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.ticker_code,
+            self.channel_name
+        )
+        super().disconnect(close_code)
 
     def price_update(self, event):
         price = event['price']
--- ccticker/ccapp/tasks.py
+++ ccticker/ccapp/tasks.py
@@ -1,4 +1,6 @@
+from asgiref.sync import async_to_sync
 from celery import shared_task
+from channels.layers import get_channel_layer
 from cryptocompy import price
 from django.core.cache import cache
 
@@ -14,7 +16,17 @@ def update_cc_prices():
     cryptocoins = ['ETH', 'BTC']
     currencies = ['EUR', 'USD']
     response = price.get_current_price(cryptocoins, currencies)
+    channel_layer = get_channel_layer()
     for cryptocoin in cryptocoins:
         for currency in currencies:
+            latest_price = response[cryptocoin][currency]
             ticker_code = cryptocoin + currency
-            cache.set(ticker_code, response[cryptocoin][currency])
+            if cache.get(ticker_code) != latest_price:
+                cache.set(ticker_code, response[cryptocoin][currency])
+                async_to_sync(channel_layer.group_send)(
+                    ticker_code,
+                    {
+                        'type': 'price_update',
+                        'price': latest_price,
+                    }
+                )
